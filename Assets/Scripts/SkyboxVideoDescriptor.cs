349e77a06c17952c5e80c5d25f9b99327a150d24
BandoleroNext <norbycasa@gmail.com>
2023-03-15 16:52:44 +0100

    Created Descriptor Script

---
+++ Assets/Scripts/SkyboxVideoDescriptor.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chapter : ScriptableObject
+{
+    public string Title = "";
+    public string Description = "";
+    public string Url = "";
+}
