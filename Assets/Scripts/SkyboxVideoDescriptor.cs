5961bc7c9ca607ba570f292b8059f399597cd2ff
BandoleroNext <norbycasa@gmail.com>
2023-03-15 17:59:08 +0100

    Descriptor Renamed and Relocated

--- Assets/Scripts/SkyboxVideoDescriptor.cs
+++ Assets/Scripts/SkyboxVideoDescriptor.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "360°video/New Descriptor")]
+[CreateAssetMenu(menuName = "360°video/New Skybox Video Descriptor")]
 public class SkyboxVideoDescriptor : ScriptableObject
 {
     public string Title = "";
