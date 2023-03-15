ce6a9fbe38338d6925ba223d46a5c8be7ef30b9b
BandoleroNext <norbycasa@gmail.com>
2023-03-15 17:14:54 +0100

    Created Descriptor Example

--- Assets/Scripts/SkyboxVideoDescriptor.cs
+++ Assets/Scripts/SkyboxVideoDescriptor.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(menuName = "360°video/New Descriptor")]
 public class SkyboxVideoDescriptor : ScriptableObject
 {
     public string Title = "";
