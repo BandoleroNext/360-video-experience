6af4f7652422c2ca795c8fec4d46649c4197cca9
BandoleroNext <norbycasa@gmail.com>
2023-03-22 10:47:56 +0100

    GameManager Created with Empty Methods

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+
+    public enum IndexOrName
+    {
+        Index,
+        Name
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void ChangeScene(int index)
+    {
+        
+    }
+    
+    private void ChangeScene(string videoName)
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
