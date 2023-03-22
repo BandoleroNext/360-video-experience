19274e8f4d9fdc7ee2315d59848c910c80aa6bff
BandoleroNext <norbycasa@gmail.com>
2023-03-22 10:57:22 +0100

    Keyboard Inputs Added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -17,18 +17,30 @@ public class GameManager : Singleton<GameManager>
         
     }
 
-    private void ChangeScene(int index)
-    {
-        
-    }
-    
-    private void ChangeScene(string videoName)
+    private static void ChangeScene(int index)
     {
         
     }
 
-    private void Update()
+    private static void ChangeScene(string videoName)
     {
         
     }
+
+#if UNITY_EDITOR
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Debug.Log("Going to Video Scene");
+            GameManager.ChangeScene("Video");
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Debug.Log("Going to Menu Scene");
+            GameManager.ChangeScene(1);
+        }
+    }
+#endif
 }
