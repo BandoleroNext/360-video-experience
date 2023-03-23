bac74a72f0b69b011172c1d83cb60e66c57b8e73
BandoleroNext <norbycasa@gmail.com>
2023-03-23 13:03:15 +0100

    Refactored Methods (Deleted Coroutine)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,31 +7,19 @@ using UnityEngine.SceneManagement;
 public class GameManager : Singleton<GameManager>
 {
 
-    public void ChangeScene(int index)
+    public void ChangeScene(int sceneIndex)
     {
-        StartCoroutine(Changing_CoroutineInd(index));
+        SkyboxVideoController.DoFadeAndCallCallback(0, () =>
+        {
+            SceneManager.LoadSceneAsync(sceneIndex, LoadSceneMode.Single);
+        });
     }
 
     public void ChangeScene(string sceneName)
     {
-        StartCoroutine(Changing_CoroutineName(sceneName)); 
-    }
-
-    private static IEnumerator Changing_CoroutineInd(int index)
-    {
-        yield return null;
-        SkyboxVideoController.DoFadeAndCallCallback(0, () =>
-        {
-            SceneManager.LoadSceneAsync(index, LoadSceneMode.Single);
-        });
-    }
-    
-    private static IEnumerator Changing_CoroutineName(string sceneName)
-    {
-        yield return null;
         SkyboxVideoController.DoFadeAndCallCallback(0, () =>
         {
             SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single);
-        });
+        }); 
     }
 }
