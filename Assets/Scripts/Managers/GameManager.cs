12ea9ea6b1616e7d163728e2788fbe909f28bec7
Marcello Bertoli <marcello.bertoli@uqido.com>
2023-03-27 18:03:51 +0200

    Fix issues commented in merge request

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -9,7 +9,7 @@ public class GameManager : Singleton<GameManager>
     [SerializeField] private float changeSceneFadeTime = 0.2f; 
     public void ChangeScene(int sceneIndex)
     {
-        VideoControllerWithInterruptions.DoFadeAndCallCallback(0, () =>
+        InterruptibleVideoController.DoFadeAndCallCallback(0, () =>
         {
             SceneManager.LoadSceneAsync(sceneIndex, LoadSceneMode.Single);
         }, changeSceneFadeTime);
@@ -17,7 +17,7 @@ public class GameManager : Singleton<GameManager>
 
     public void ChangeScene(string sceneName)
     {
-        VideoControllerWithInterruptions.DoFadeAndCallCallback(0, () =>
+        InterruptibleVideoController.DoFadeAndCallCallback(0, () =>
         {
             SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single);
         }, changeSceneFadeTime); 
