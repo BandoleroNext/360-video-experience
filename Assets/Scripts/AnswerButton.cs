f5e62df70544fee11bf9c73e936479a8cb4ec8ef
BandoleroNext <norbycasa@gmail.com>
2023-03-28 16:39:41 +0200

    Add Sound Response (Right and Wrong)

--- Assets/Scripts/AnswerButton.cs
+++ Assets/Scripts/AnswerButton.cs
@@ -3,20 +3,36 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Descriptors;
+using Oculus.Interaction;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AnswerButton : MonoBehaviour
 {
     [SerializeField] private QuizInterruptionDescriptor _quizInterruptionDescriptor;
+    private Answer _testValue;
+    
     void Start()
     {
-        var answer = _quizInterruptionDescriptor.answers.First().answer;
-        GetComponentInChildren<TextMeshPro>().text = answer;
+        _testValue = _quizInterruptionDescriptor.answers.First();
+        Setup();
     }
     
-    void Update()
+    private void Setup()
     {
-        
+        UpdateText();
+        UpdateCallBack();
+    }
+
+    private void UpdateText()
+    {
+        var answer = _testValue.answer;
+        GetComponentInChildren<TextMeshPro>().text = answer;
+    }
+
+    private void UpdateCallBack()
+    {
+        transform.Find("Audio/ButtonRelease").GetComponent<AudioSource>().clip = _testValue.isCorrect ? GameManager.Instance.rightAnswerSound : GameManager.Instance.wrongAnswerSound;
     }
 }
