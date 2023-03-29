84ad55e4c822a268677f69a46ec55c38aa2c5350
BandoleroNext <norbycasa@gmail.com>
2023-03-29 17:39:44 +0200

    Set Prefab with Select

--- Assets/Scripts/QuizController.cs
+++ Assets/Scripts/QuizController.cs
@@ -6,13 +6,5 @@ using UnityEngine;
 
 public class QuizController : MonoBehaviour
 {
-    [SerializeField] private QuizInterruptionDescriptor quizInterruptionDescriptor;
-    private Answer _testValue;
-    
-    
-    void Start()
-    {
-        _testValue = quizInterruptionDescriptor.answers[1];
-        GameObject.Find("ExampleAnswerButton").GetComponent<AnswerButton>().Setup(_testValue);
-    }
+
 }
