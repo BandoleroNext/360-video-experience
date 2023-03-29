7efe219f1811b1c3dd77dc4719549df5fe3150ed
BandoleroNext <norbycasa@gmail.com>
2023-03-29 16:08:43 +0200

    Set Position of Each Possibility

--- Assets/Scripts/QuizController.cs
+++ Assets/Scripts/QuizController.cs
@@ -7,26 +7,55 @@ using UnityEngine;
 public class QuizController : MonoBehaviour
 {
     [SerializeField] private QuizInterruptionDescriptor quizInterruptionDescriptor;
-    private Answer _testValue;
     public GameObject answerButtonPrefab;
+    private List<GameObject> _listOfAnswerButtons;
 
     void Start()
     {
         var answers = quizInterruptionDescriptor.answers;
-        foreach (var singleAnswer in answers )
+        _listOfAnswerButtons = new List<GameObject>();
+        foreach (var singleAnswer in answers)
         {
             CreateAndSetEachButton(singleAnswer);
         }
-        
+        PlaceAnswersIntoScene();
     }
 
 
     void CreateAndSetEachButton(Answer singleAnswer)
     {
-        var answer1 = Instantiate(answerButtonPrefab, new Vector3(0, 1, 0.335f), Quaternion.identity);
-        answer1.name = "Answer1";
-        _testValue = quizInterruptionDescriptor.answers[0];
-        GameObject.Find("Answer1").GetComponent<AnswerButton>().Setup(_testValue); 
+        var answerButton = Instantiate(answerButtonPrefab, new Vector3(0, 1, 0.335f), Quaternion.identity);
+        answerButton.name = "Answer";
+        _listOfAnswerButtons.Add(answerButton);
+        answerButton.GetComponent<AnswerButton>().Setup(singleAnswer);
+    }
+    
+    private void PlaceAnswersIntoScene()
+    {
+        var twoOptionsLocalPositions = new[]
+            {new Vector3(-0.25f,1,0.5f), new Vector3(0.25f,1,0.5f)};
+        
+        var threeOptionsLocalPositions = new[]
+            {new Vector3(-0.25f, 1.2f, 0.5f), new Vector3(0.25f, 1.2f, 0.5f), new Vector3(0, 0.7f, 0.5f)};
+
+        var fourOptionsLocalPositions = new[]
+        {
+            new Vector3(-0.25f, 1.2f, 0.5f), new Vector3(0.25f, 1.2f, 0.5f),
+            new Vector3(-0.25f, 0.7f, 0.5f), new Vector3(0.25f, 0.7f, 0.5f)
+        };
+
+        var positions = _listOfAnswerButtons.Count switch
+        {
+            2 => twoOptionsLocalPositions,
+            3 => threeOptionsLocalPositions,
+            _ => fourOptionsLocalPositions
+        };
+
+        for (var i = 0; i < _listOfAnswerButtons.Count; i++)
+        {
+            var answer = _listOfAnswerButtons[i];
+            answer.transform.localPosition = positions[i];
+        }
     }
     
 }
