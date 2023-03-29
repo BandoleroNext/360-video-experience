c508f582833853fa76a66864aaa4bdc4fc184690
BandoleroNext <norbycasa@gmail.com>
2023-03-29 11:09:57 +0200

    QuizController Create One Button

--- Assets/Scripts/QuizController.cs
+++ Assets/Scripts/QuizController.cs
@@ -8,11 +8,13 @@ public class QuizController : MonoBehaviour
 {
     [SerializeField] private QuizInterruptionDescriptor quizInterruptionDescriptor;
     private Answer _testValue;
-    
-    
+    public GameObject answerButtonPrefab;
+
     void Start()
     {
-        _testValue = quizInterruptionDescriptor.answers[1];
-        GameObject.Find("ExampleAnswerButton").GetComponent<AnswerButton>().Setup(_testValue);
+        var answer1 = Instantiate(answerButtonPrefab, new Vector3(0, 1, 0.335f), Quaternion.identity);
+        answer1.name = "Answer1";
+        _testValue = quizInterruptionDescriptor.answers[0];
+        GameObject.Find("Answer1").GetComponent<AnswerButton>().Setup(_testValue);
     }
 }
